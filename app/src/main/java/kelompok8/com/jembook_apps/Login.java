e60a065e63d583afed0b69d1f53995cc97f9e2b3
miftahfajari <miftah.fajar25@gmail.com>
2018-03-27 07:55:19 +0700

    Master

---
+++ app/src/main/java/kelompok8/com/jembook_apps/Login.java
@@ -0,0 +1,90 @@
+package kelompok8.com.jembook_apps;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login extends AppCompatActivity {
+
+    private EditText mEmail, mPassword;
+    private Button mLogin;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mEmail = (EditText) findViewById(R.id.email);
+        mPassword = (EditText) findViewById(R.id.password);
+        mLogin = (Button) findViewById(R.id.login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                if (firebaseAuth.getCurrentUser() != null ) {
+
+                    startActivity(new Intent(Login.this, Home.class));
+
+                }
+            }
+        };
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startSignIn();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    private void startSignIn() {
+
+        String email = mEmail.getText().toString();
+        String password = mPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(Login.this, "Email Tidak Boleh Kosong", Toast.LENGTH_LONG).show();
+            return;
+        } else if (TextUtils.isEmpty(password)) {
+            Toast.makeText(Login.this, "Password Tidak Boleh Kosong", Toast.LENGTH_LONG).show();
+            return;
+        } else {
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    if (!task.isSuccessful()) {
+                        Toast.makeText(Login.this, "Sign In Problem", Toast.LENGTH_LONG).show();
+
+                    }
+                }
+            });
+        }
+
+    }
+}
---
+++ app/src/main/java/kelompok8/com/jembook_apps/Splash.java
@@ -0,0 +1,48 @@
+package kelompok8.com.jembook_apps;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class Splash extends AppCompatActivity {
+
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_splash);
+
+        tv = (TextView) findViewById(R.id.judul_splash);
+
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.mytransition);
+        tv.startAnimation(myanim);
+
+
+        final Intent i = new Intent(Splash.this, Login.class);
+
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+}
