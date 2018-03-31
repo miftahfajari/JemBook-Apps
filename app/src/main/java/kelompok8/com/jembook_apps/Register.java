506292edd51216db9427545d020ef5b5bd941bbf
arfiaari <arfia.ari@gmail.com>
2018-03-31 10:55:11 +0700

    register done

--- app/src/main/java/kelompok8/com/jembook_apps/Register.java
+++ app/src/main/java/kelompok8/com/jembook_apps/Register.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -13,11 +14,14 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Register extends AppCompatActivity {
 
     private EditText inputEmail, inputPassword;
+    private Button btnRegis;
     private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +33,39 @@ public class Register extends AppCompatActivity {
 
         inputEmail = (EditText) findViewById(R.id.reg_email);
         inputPassword = (EditText) findViewById(R.id.reg_pass);
-    }
-    public void register(View view) {
+        btnRegis = (Button) findViewById(R.id.reg_btn);
 
-        String email = inputEmail.getText().toString().trim();
-        String password = inputPassword.getText().toString().trim();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                if (firebaseAuth.getCurrentUser() != null ) {
+
+                    startActivity(new Intent(Register.this, Login.class));
+
+                }
+            }
+        };
+
+        btnRegis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                register();
+            }
+        });
+
+
+    }
+
+    public void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(mAuthListener);
+    }
+
+    private void register() {
+
+        String email = inputEmail.getText().toString();
+        String password = inputPassword.getText().toString();
 
         if (TextUtils.isEmpty(email)) {
             Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
@@ -64,6 +96,7 @@ public class Register extends AppCompatActivity {
                         if (!task.isSuccessful()) {
                             Toast.makeText(Register.this, "Authentication failed." + task.getException(),
                                     Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = auth.getCurrentUser();
                         } else {
                             startActivity(new Intent(Register.this, Login.class));
                             finish();
